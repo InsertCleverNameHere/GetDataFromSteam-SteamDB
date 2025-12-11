40c318f3073a42c1e4fe1cb256531bc132b9bda4
InsertCleverNameHere <mol7d2bnws5a@gmail.com>
2025-12-11 14:25:56 +0200

    add meta file for auto-update checking

---
+++ Primary/prototype2(Go).meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         SteamDB Data Fork
+// @namespace    https://steamdb.info/
+// @version      0.2.3
+// @description  Fetches Achievements/DLCs. Generates Tenoke, Goldberg, and RUNE configs.
+// @author       SCN
+// @match        https://steamdb.info/app/*
+// @icon         https://steamdb.info/static/logos/192px.png
+// @homepage     https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @homepageURL  https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @source       github:InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @supportURL   https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/issues
+// @downloadURL  https://raw.githack.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/main/Primary/prototype2(Go).user.js
+// @updateURL    https://raw.githack.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/main/Primary/prototype2(Go).meta.js
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/fflate@0.8.0/umd/index.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setClipboard
+// ==/UserScript==
--- Primary/prototype2(Go).user.js
+++ Primary/prototype2(Go).user.js
@@ -6,6 +6,12 @@
 // @author       SCN
 // @match        https://steamdb.info/app/*
 // @icon         https://steamdb.info/static/logos/192px.png
+// @homepage     https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @homepageURL  https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @source       github:InsertCleverNameHere/GetDataFromSteam-SteamDB
+// @supportURL   https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/issues
+// @downloadURL  https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/main/Primary/prototype2(Go).user.js
+// @updateURL    https://github.com/InsertCleverNameHere/GetDataFromSteam-SteamDB/main/Primary/prototype2(Go).meta.js
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/fflate@0.8.0/umd/index.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
